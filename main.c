6cd580f499473452c5d5e9886072c4551f8e4b1e
Saleh Abdulla <salehabdullaict@gmail.com>
2025-10-29 22:58:08 +0300

    using placeholder with printf

--- main.c
+++ main.c
@@ -7,5 +7,13 @@ int main() {
     printf("I like pizza!\n");
     printf("It's really good\n");
 
+    float age = 25.5;
+    int year = 2025;
+    float price = 33.33;
+
+    printf("I'm %.1f years old\n", age);
+    printf("The year is %d. ", year);
+    printf("The price is $%f\n", price);
+
     return 0;
 }
